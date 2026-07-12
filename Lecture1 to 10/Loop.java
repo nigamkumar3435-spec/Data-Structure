1a2fdd0657adec622a6b3cdc436b3f7e3718a048
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-07-12 23:18:17 +0530

    Add files via upload

--- Lecture1 to 10/Loop.java
+++ Lecture1 to 10/Loop.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
+public class Loop{
 
+  public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter the number: ");
+      int n=sc.nextInt();
+
+      while(n!=0)
+      {
+        n=n/2;
+        
+        if(n%2==0) {
+          continue;
+        }
+        System.out.println(n);      
+      }
+      sc.close();
+    }
+  
+  
+}
