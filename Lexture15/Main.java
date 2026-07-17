11904ff07c2727a56ceada7a1e9ff8d7a67efaaf
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-07-17 22:46:56 +0530

    add code for object and class

--- Lexture15/Main.java
+++ Lexture15/Main.java
@@ -1,1 +1,27 @@
+class Car
+{
+    String model; //instance field
+    private int speed;
 
+    void accelerate(int delta) //behaviour
+    {
+        speed+=delta;
+    }
+
+    int getSpeed()
+    {
+        return speed;
+    }
+}
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // objects are initialized or stored in heap memory.and variables refer that object.
+        // variables are stored in stack memeory.
+        Car car = new Car();
+        car.model = "BMW";
+        car.accelerate(60);
+        System.out.println(car.getSpeed());
+    }
+}
