8fc9e669d30afb3dd2c9c0912a056ced05929967
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-07-18 21:31:44 +0530

    Add code for Single Inheritance

--- Lexture19/SingleInheritance.java
+++ Lexture19/SingleInheritance.java
@@ -1,1 +1,29 @@
+class Animal
+{
+    void eat()
+    {
+        System.out.println("Animal is eating");
+    }
+}
 
+class Dog extends Animal
+{
+    void bark()
+    {
+        System.out.println("Dog is barking");
+    }
+}
+
+public class SingleInheritance
+{
+    public static void main(String[] args)
+    {
+        Dog d = new Dog();
+        d.eat(); //from Animal
+        d.bark(); //from Dog
+
+        Animal a = new Animal();
+        a.eat();
+        //a.bark(); //parent class cannot access child methods.
+    }
+}
